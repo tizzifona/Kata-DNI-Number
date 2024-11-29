6131f4d28fb18d29ed9f74db548d1ad81a1b79d2
tizzifona <tizzifona@gmail.com>
2024-11-29 08:51:26 +0100

    Added DNIModel

--- src/main/java/projects/f5/dni_number_java/model/DNIModel.java
+++ src/main/java/projects/f5/dni_number_java/model/DNIModel.java
@@ -1,5 +1,15 @@
 package projects.f5.dni_number_java.model;
 
 public class DNIModel {
+    private static final char[] LETTERS = {
+            'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B',
+            'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'
+    };
 
+    public char calculateNIFLetter(int dniNumber) {
+        if (dniNumber < 0 || dniNumber > 99999999) {
+            throw new IllegalArgumentException("DNI number must be between 0 and 99999999.");
+        }
+        return LETTERS[dniNumber % 23];
+    }
 }
